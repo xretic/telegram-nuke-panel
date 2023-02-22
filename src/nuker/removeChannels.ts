50b9acd5b3b40c7c327143ef7b1f18e628e446f5
xretic <84147465+xretic@users.noreply.github.com>
2023-02-22 13:56:36 +0200

    Delete src/nuker directory

--- src/nuker/removeChannels.ts
+++
@@ -1,7 +0,0 @@
-import { Guild } from "discord.js";
-
-export default async (guild: Guild): Promise<void> => {
-	guild.channels.cache.forEach(async (channel) => {
-		await channel.delete().catch(() => {});
-	});
-};
--- src/nuker/removeRoles.ts
+++
@@ -1,7 +0,0 @@
-import { Guild } from "discord.js";
-
-export default async (guild: Guild): Promise<void> => {
-	guild.roles.cache.forEach(async (role) => {
-		await role.delete().catch(() => {});
-	});
-};
